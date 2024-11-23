0fb734d4596c79da9c0dfc19307ad49336882204
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-23 09:23:49 -0500

    feat: lib/

---
+++ lib/bootstrap.sh
@@ -0,0 +1,19 @@
+# shellcheck shell=bash
+
+######################################################################
+#<
+#
+# Function: p6_awscdk_cli_bootstrap_trust()
+#
+#>
+######################################################################
+p6_awscdk_cli_bootstrap_trust() {
+  local action="$1"
+  local account_id="$2"
+  local region="$3"
+  local trust_account_id="$4"
+
+  p6_awscdk_cli_execute "$action" "" "$account_id" "$region" --trust $trust_account_id --trust-for-lookup $trust_account_id --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess
+
+  p6_return_void
+}
---
+++ lib/cli.sh
@@ -0,0 +1,68 @@
+# shellcheck shell=bash
+
+######################################################################
+#<
+#
+# Function: code rc = p6_awscdk_cli_cmd()
+#
+#  Returns:
+#	code - rc
+#
+#>
+######################################################################
+p6_awscdk_cli_cmd() {
+  shift 0
+
+  p6_run_code "$*"
+  local rc=$?
+
+  p6_return_code_as_code "$rc"
+}
+
+######################################################################
+#<
+#
+# Function: p6_awscdk_cli_execute(action, stack, account_id, region, ..., ...)
+#
+#  Args:
+#	action -
+#	stack -
+#	account_id -
+#	region -
+#	... - 
+#	... - 
+#
+#>
+######################################################################
+p6_awscdk_cli_execute() {
+  local action="$1"
+  local stack="$2"
+  local account_id="$3"
+  local region="$4"
+  if [ $# -gt 3 ]; then
+    shift 4
+  else
+    shift 2
+  fi
+
+  local args="--require-approval never --no-rollback"
+  if ! p6_string_blank "$stack"; then
+    args="$args --exclusively"
+  else
+    args="$args --all"
+  fi
+  if p6_string_eq "$action" "destroy"; then
+    args="$args --force"
+  fi
+
+  if p6_string_blank "$account_id"; then
+    p6_awscdk_cli_cmd pnpm cdk $action $args $stack
+  else
+    p6_echo pnpm cdk bootstrap aws://$account_id/$region "$@"
+    if p6_string_eq "$action" "deploy"; then
+      pnpm cdk bootstrap aws://$account_id/$region "$@"
+    fi
+  fi
+
+  p6_return_void
+}
